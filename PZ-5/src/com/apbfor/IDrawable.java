76b3c594e201df6703cdecfbebd765285d6313f2
apbfor <apbforgame@gmail.com>
2019-05-20 20:58:25 +0300

    init pz5

---
+++ PZ-5/src/com/apbfor/IDrawable.java
@@ -0,0 +1,5 @@
+package com.apbfor;
+
+interface IDrawable {
+    void draw();
+}
---
+++ PZ-5/src/com/apbfor/Point.java
@@ -0,0 +1,56 @@
+package com.apbfor;
+
+public class Point implements IDrawable {
+    private double x, y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Point() {
+        x = y = 0;
+    }
+
+    public void Set(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+
+    public String toString1(){
+        return "This is point. X = " + x + "; Y = " + y;
+    }
+
+    @Override
+    public boolean equals(Object second) {
+        if (!second.getClass().isInstance(new Point())||second==null)
+            return false;
+        return this.x == ((Point)second).x && this.y == ((Point)second).y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void draw() {
+        System.out.println("This is Point, x = " + x + "; y = " + y);
+    }
+
+
+    public int compareTox(Point o) {
+        if (this.getX() < o.getX())
+            return -1;
+        else if (this.getX() > o.getX())
+            return 1;
+        return 0;
+    }
+}
